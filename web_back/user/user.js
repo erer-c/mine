cbdfa43ae1e0cc1d0b69dc8e27dd787e5e39d822
unknown <1799684417@qq.com>
2019-11-20 22:53:11 +0800

    获取用户信息

--- web_back/user/user.js
+++ web_back/user/user.js
@@ -1,6 +1,7 @@
 // 语义化
 var url = "http://localhost:8000";
 var user = {
+    // 登录请求
     login: function(myName, myPassword) {
 
         $.post(url + "/admin/login", {
@@ -17,6 +18,7 @@ var user = {
             }
         })
     },
+    // 退出请求
     quit: function() {
         $.post(url + '/admin/logout', function(res) {
             console.log(1);
@@ -24,6 +26,21 @@ var user = {
             if (res.code === 200) {
                 window.location.href = "./login.html";
 
+            }
+        })
+    },
+
+    // 获取用户信息请求
+    gain: function() {
+        $.get(url + "/admin/getuser", function(res) {
+            console.log(res)
+                // console.log(res.data.user_pic);
+            console.log($("#loginImg"));
+
+
+            if (res.code === 200) {
+                $("#loginName").text(res.data.nickname);
+                $("#loginImg").prop('src', res.data.user_pic);
             }
         })
     }
