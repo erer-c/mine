be8105097ce050e59c234c54c63246d66add72bc
unknown <1799684417@qq.com>
2019-11-22 10:35:12 +0800

    查，增，删，改完成

---
+++ web_back/user/category.js
@@ -0,0 +1,41 @@
+var category = {
+    // 获取并展示数据
+    gain: function(callback) {
+        $.get(userurl.category_search, function(res) {
+            console.log(res);
+            callback(res)
+        })
+    },
+
+    // 添加数据
+    add: function(name, slug, callback) {
+        $.post(userurl.category_add, { "name": name, "slug": slug }, function(res) {
+            console.log(1);
+            callback(res);
+
+        })
+    },
+
+    // 编辑数据
+    edit: function(id, name, slug, callback) {
+        $.post(userurl.category_edit, { "id": id, "name": name, "slug": slug }, function(res) {
+            console.log(res);
+
+            callback(res);
+
+        })
+    },
+
+
+    // 删除数据
+    delete: function(id, callback) {
+        $.post(userurl.category_delete, { "id": id }, function(res) {
+            console.log(res);
+
+            callback(res);
+
+        })
+    },
+
+
+}
--- web_back/user/url.js
+++ web_back/user/url.js
@@ -3,4 +3,10 @@ var userurl = {
     userurl_login: url + "admin/login",
     userurl_logout: url + 'admin/logout',
     userurl_getuser: url + "admin/getuser",
+
+    category_search: url + "admin/category_search",
+    category_add: url + "admin/category_add",
+    category_edit: url + "admin/category_edit",
+    category_delete: url + "admin/category_delete",
+
 }
