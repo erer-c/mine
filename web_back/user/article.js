f21829019fee38522f3712ef399f224e525b1182
unknown <1799684417@qq.com>
2019-11-24 23:51:56 +0800

    发表文章

--- web_back/user/article.js
+++ web_back/user/article.js
@@ -1,7 +1,11 @@
 var article = {
     // 获取数据
-    gain: function(callback) {
-        $.get(userurl.article_search,
+    gain: function(curPage, type, state, callback) {
+        $.get(userurl.article_search, {
+                page: curPage,
+                type: type,
+                state: state
+            },
             function(res) {
                 // console.log(res);
                 callback(res);
@@ -11,9 +15,21 @@ var article = {
     // 删除数据
     delete: function(id, callback) {
         $.get(userurl.article_delete, { id: id }, function(res) {
-            console.log(res);
+            // console.log(res);
             callback(res);
         })
     },
+    publish: function(fd, callback) {
+        $.ajax({
+            url: userurl.article_add,
+            type: "post",
+            data: fd,
+            processData: false, //不要jQuery处理formdata数据
+            contentType: false, //不要使用默认请求头
+            success: function(res) {
+                callback(res);
+            }
+        })
+    }
 
 }
